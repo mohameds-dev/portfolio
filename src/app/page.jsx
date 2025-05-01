95211e4a28a09f1226f87336a3f10f94c5986415
itsmosalah <mosalah3480@gmail.com>
2025-04-30 23:48:16 -0500

    Refactor to jsx, create dummy page with component

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <main className="text-center mt-32 text-2xl">
+      <Header/>
+      <p className="text-lg text-gray-600 dark:text-gray-300 mt-2">Portfolio coming soon.</p>
+    </main>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h1 className="font-bold">Mohamed A.</h1>
+  )
+}
+
+export default Header
