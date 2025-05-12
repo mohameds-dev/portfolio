cc369afa11ecb28b5ce42dbf0f02ff560a6ae7c4
itsmosalah <mosalah3480@gmail.com>
2025-05-11 23:14:15 -0500

    feat: add hero section initial code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,9 @@
-import Header from "@/components/Header";
-import Link from "next/link";
 import ProjectsSection from "@/components/projects/ProjectsSection";
-
+import HeroSection from "@/components/hero/HeroSection";
 export default function Home() {
   return (
     <main className="text-center mt-32 text-2xl">
-      <Header />
+      <HeroSection />
       <ProjectsSection />
     </main>
   );
---
+++ src/components/hero/HeroSection.jsx
@@ -0,0 +1,15 @@
+export default function HeroSection() {
+  return (
+    <section className="py-12 px-4 md:px-8">
+      <div className="max-w-7xl mx-auto">
+        <h2 className="text-3xl font-bold text-gray-900 dark:text-white mb-8">
+          Mohamed Abdelrahman
+        </h2>
+        <p className="text-gray-600 dark:text-gray-400 mb-4">
+          I am a Problem Solver, a Developer, an Engineer, and a Student.
+        </p>
+      </div>
+    </section>
+  );
+}
+
